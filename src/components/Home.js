9ac36cc57cfa98651bb35732b6a877660d0d2351
Alexander <elementoffchaos@gmail.com>
2019-11-20 17:43:22 +0700

    video palyer playbutton and bar added

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,15 @@ import { getSpecials } from '../helpers';
 
 import Carousel from './Carousel';
 import LogoSimple from '../svg/LogoSimple';
-import AnimBtn from './AnimBtn';
 
 const Home = () => {
   const [videoPlay, setVideoPlay] = useState(false);
   const [hovered, setHovered] = useState(false);
+  const [videoTime, setVideoTime] = useState(0);
 
-  const videoRef = useRef(null);
+  const videoRef = useRef({ current: { currentTime: 0, duration: 10 } });
+
+  const videoDur = videoRef.current.duration || 10;
 
   const products = useSelector(state => state.products);
 
@@ -29,9 +31,14 @@ const Home = () => {
     setHovered(false);
   };
 
+  const handleTime = e => {
+    const newTime = e.target.currentTime;
+    setVideoTime(newTime);
+  };
+
   useEffect(() => {
     videoPlay ? videoRef.current.play() : videoRef.current.pause();
-  }, [videoPlay]);
+  }, [videoPlay, videoTime]);
 
   return (
     <div className='home'>
@@ -55,6 +62,7 @@ const Home = () => {
               onClick={handlePlayVideo}
               onMouseOver={handleHoverVideo}
               onMouseLeave={handleUnhoverVideo}
+              onTimeUpdate={handleTime}
             >
               <source src='/img/it-was-all-yellow.mp4' type='video/mp4' />
               <source src='/img/it-was-all-yellow.ogv' type='video/ogg' />
@@ -62,22 +70,20 @@ const Home = () => {
               Sorry, your browser doesn't support embedded videos
             </video>
 
-            {!videoPlay && (
-              <div className='controls'>
-                <div className='bar'>
-                  <div className='bar__content'></div>
-                </div>
-                <div className='buttons'>
-                  <AnimBtn
-                    className='play'
-                    onClick={handlePlayVideo}
-                    showArrow={false}
-                  >
-                    Play
-                  </AnimBtn>
-                </div>
+            <div className='controls'>
+              {' '}
+              <div className='buttons'>
+                <button className='play' onClick={handlePlayVideo}>
+                  {videoPlay ? 'Pause' : 'Play'}
+                </button>
               </div>
-            )}
+              <div className='bar'>
+                <div
+                  className='bar__content'
+                  style={{ width: `${(videoTime / videoDur) * 100}%` }}
+                ></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
