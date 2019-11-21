db1d9302390eda93cbed54b404d89d8f8bb90be5
Alexander <elementoffchaos@gmail.com>
2019-11-21 19:24:49 +0700

    Updates

---
+++ precache-manifest.3d118dd5ded5eb71d423329b68eb2604.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157628356c180a2e51134d0891053046",
+    "url": "/gardenshop/index.html"
+  },
+  {
+    "revision": "b837d29c258e5095d77b",
+    "url": "/gardenshop/static/css/main.d1f946b9.chunk.css"
+  },
+  {
+    "revision": "0bb9204e49a2dcaab52e",
+    "url": "/gardenshop/static/js/2.343a283f.chunk.js"
+  },
+  {
+    "revision": "b837d29c258e5095d77b",
+    "url": "/gardenshop/static/js/main.69f538b6.chunk.js"
+  },
+  {
+    "revision": "d81e1809a67bc65c784a",
+    "url": "/gardenshop/static/js/runtime-main.1ddcc524.js"
+  },
+  {
+    "revision": "fc405b78baf0b6ccf4466d9d79180733",
+    "url": "/gardenshop/static/media/fertilizers.fc405b78.jpg"
+  },
+  {
+    "revision": "162c26d6491abecfff7d034baaf6dca6",
+    "url": "/gardenshop/static/media/flowers.162c26d6.jpg"
+  },
+  {
+    "revision": "02f983429bc0ab02d86a847fd559b2f4",
+    "url": "/gardenshop/static/media/seeds.02f98342.jpg"
+  },
+  {
+    "revision": "a5d9402065f65751daf683f17b0cce51",
+    "url": "/gardenshop/static/media/tools.a5d94020.jpg"
+  }
+]);
