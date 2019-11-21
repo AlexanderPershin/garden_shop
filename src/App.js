4cdd93ff02592d5515e753fc69ba98fe695c385d
Alexander <elementoffchaos@gmail.com>
2019-11-21 19:33:43 +0700

    hash router used

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { loadData } from './actions';
 import './scss/style.scss';
